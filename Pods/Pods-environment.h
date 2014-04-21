dc53013376bd533ed8f771c5e7c81a34a5be2011
Rishabh Tayal <rtayal11@gmail.com>
2014-04-21 13:56:32 -0500

    Added blurred view support

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -6,6 +6,12 @@
 // project.
 
 
+// BlurryModalSegue
+#define COCOAPODS_POD_AVAILABLE_BlurryModalSegue
+#define COCOAPODS_VERSION_MAJOR_BlurryModalSegue 1
+#define COCOAPODS_VERSION_MINOR_BlurryModalSegue 0
+#define COCOAPODS_VERSION_PATCH_BlurryModalSegue 0
+
 // CSNotificationView
 #define COCOAPODS_POD_AVAILABLE_CSNotificationView
 #define COCOAPODS_VERSION_MAJOR_CSNotificationView 0
@@ -17,4 +23,16 @@
 #define COCOAPODS_VERSION_MAJOR_MFSideMenu 0
 #define COCOAPODS_VERSION_MINOR_MFSideMenu 5
 #define COCOAPODS_VERSION_PATCH_MFSideMenu 4
+
+// MZAppearance
+#define COCOAPODS_POD_AVAILABLE_MZAppearance
+#define COCOAPODS_VERSION_MAJOR_MZAppearance 1
+#define COCOAPODS_VERSION_MINOR_MZAppearance 1
+#define COCOAPODS_VERSION_PATCH_MZAppearance 2
+
+// UIImage+BlurredFrame
+#define COCOAPODS_POD_AVAILABLE_UIImage_BlurredFrame
+#define COCOAPODS_VERSION_MAJOR_UIImage_BlurredFrame 0
+#define COCOAPODS_VERSION_MINOR_UIImage_BlurredFrame 0
+#define COCOAPODS_VERSION_PATCH_UIImage_BlurredFrame 4
 
