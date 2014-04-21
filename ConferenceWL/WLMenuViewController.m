a418c580b1caeb4e4ecd38efbbd3d0f9c80d2f73
Rishabh Tayal <rtayal11@gmail.com>
2014-04-21 17:16:57 -0500

    commit

--- ConferenceWL/WLMenuViewController.m
+++ ConferenceWL/WLMenuViewController.m
@@ -16,6 +16,7 @@
 //#import "WMSPRViewController.h"
 #import "MFSideMenu.h"
 #import "FXBlurView.h"
+#import "UIImageView+AFNetworking.h"
 
 @interface WLMenuViewController ()
 
@@ -125,7 +126,8 @@
 {
     UIView* view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 100)];
     UIImageView* iv = [[UIImageView alloc] initWithFrame:CGRectMake(0, 30, 80, 80)];
-    iv.image = [UIImage imageNamed:@"profile-placeholder-75"];
+    [iv setImageWithURL:[NSURL URLWithString:[[NSUserDefaults standardUserDefaults] objectForKey:@"photoURL"]] placeholderImage:[UIImage imageNamed:@"profile-placeholder-75"]];
+//    iv.image = [UIImage imageNamed:@"profile-placeholder-75"];
     
     CGPoint center;
     center.x = view.center.x;
